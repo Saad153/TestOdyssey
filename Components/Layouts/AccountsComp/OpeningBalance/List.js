a3ab3ec7793b14eb0f9b11c1f0da1face126003b
Bilal Azfar <bscs2012242@szabist.pk>
2024-11-12 17:29:09 +0500

    Added delete in opening balances

--- Components/Layouts/AccountsComp/OpeningBalance/List.js
+++ Components/Layouts/AccountsComp/OpeningBalance/List.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Col, Row, Table } from "react-bootstrap";
 import Router from 'next/router';
 import moment from 'moment';
+import axios from 'axios';
+import { DeleteOutlined } from '@ant-design/icons';
+import PopConfirm from '/Components/Shared/PopConfirm';
+import Cookies from 'js-cookie';
+
 
 const OpeningBalance = ({sessionData, openingBalancesList}) => {
 
@@ -10,6 +15,26 @@ const OpeningBalance = ({sessionData, openingBalancesList}) => {
   useEffect(() => {
     setRecords(openingBalancesList.result)
   }, [])
+
+  const getData = async() => {
+    let openingBalances = await axios.get(process.env.NEXT_PUBLIC_CLIMAX_GET_OPENING_BALANCES,{
+      headers:{"id": `${Cookies.get('companyId')}`}
+    }).then((x)=>x.data);
+    setRecords(openingBalances.result)
+  }
+
+  const handleDelete = (id) => {
+    console.log(id)
+    PopConfirm("Confirmation", "Are You Sure To Remove This Charge?",
+    async () => {
+      console.log(id)
+      const result = await axios.post(process.env.NEXT_PUBLIC_CLIMAX_POST_DELETE_BASE_VOUCHER, {
+        id: id
+      })
+      console.log(result)
+      getData();
+    })
+  };
 
   return (
   <div className='base-page-layout'>
@@ -33,20 +58,22 @@ const OpeningBalance = ({sessionData, openingBalancesList}) => {
           <th>Currency</th>
           <th>Ex. Rate</th>
           <th>Date</th>
+          <th>Delete</th>
         </tr>
       </thead>
       <tbody>
       {records?.map((x, index) => {
       return (
       <tr key={index} className='f table-row-center-singleLine row-hov'
-        onClick={()=>Router.push(`/accounts/openingBalance/${x.id}`)}
+        
       >
         <td>{x?.voucher_No}</td>
-        <td>{x?.voucher_Id}</td>
+        <td onClick={()=>Router.push(`/accounts/openingBalance/${x.id}`)}>{x?.voucher_Id}</td>
         <td>{x?.costCenter}</td>
         <td>{x?.currency}</td>
         <td>{x.exRate}</td>
         <td>{moment(x?.createdAt).format("YYYY-MM-DD")}</td>
+        <td><DeleteOutlined onClick={(e) => { e.stopPropagation(); handleDelete(x.id) }} /></td>
       </tr>
       )})}
       </tbody>
